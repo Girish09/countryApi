8e4327a2a0a3bf7d3eea4dd8945c70d1ec01874a
Girish <girishkj009@gmail.com>
2021-02-22 07:38:44 +0530

    Add files via upload

--- rest-countries.js
+++ rest-countries.js
@@ -1,30 +1,28 @@
-//HTTP (hypertext transfer protocol)
-//Request - what do we want to do
-//Response what was actually done.
 
 const getCountry = (countryCode) => {
-    return new Promise ((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.addEventListener('readystatechange', (e) =>{
-            if (e.target.readyState === 4 && e.target.status === 200){
-                 const data = JSON.parse(e.target.responseText);
-                 const country = data.find((country)=>country.alpha2Code === countryCode);
-                 resolve(country.name);       
-                
-            } else if (e.target.readyState === 4){
-                reject("An error has taken place");
-            }
-        });
-        
-        request.open('GET', 'https://restcountries.eu/rest/v2/all');
-        request.send();
-    })
+  return fetch('https://restcountries.eu/rest/v2/all').then((response) => {
+    if (response.status === 200){
+      return response.json().then((data) => {
+          const country = data.find((country)=>country.alpha2Code === countryCode);
+            return country;
+      })    
+    } else {
+      throw new Error ('Unable to fetch the puzzle')
+    }
+}).then((data) => {
+    return data.name
+})
+
 }
 
-getCountry('US').then((resolvedData) => {
-  console.log(`Country name: ${resolvedData}`);
+getCountry('PK').then((data) => {
+  console.log(data);
 }).catch((err) => {
-  console.log(err);
+  console.log(`Error: ${err}`);
 });
+
+
+
+
 
 
